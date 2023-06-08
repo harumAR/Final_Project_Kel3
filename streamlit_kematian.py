a50e7bfe3fb6dbf46d65af422ba4fb4ecb890527
Harum Aprelina R <104288297+harumAR@users.noreply.github.com>
2023-06-09 02:59:12 +0700

    streamlit_kematian.py

--- streamlit_kematian.py
+++ streamlit_kematian.py
@@ -26,89 +26,89 @@ AGE = st.text_input("AGE")
 if AGE != '':
     AGE = float(AGE)  # Convert to float
 
-USMER = st.radio("USMER", ['First', 'Second', 'Third'])
-if USMER == 'First':
+USMER = st.radio("Tingkat Unit Medis", ['Pertama', 'Kedua', 'Ketiga'])
+if USMER == 'Pertama':
     USMER = 1
-elif USMER == 'Second':
+elif USMER == 'Kedua:
     USMER = 2
-elif USMER == 'Third':
+elif USMER == 'Ketiga':
     USMER = 3
 
-MEDICAL_UNIT = MEDICAL_UNIT = st.slider("MEDICAL_UNIT", 0, 13, step=1)
+MEDICAL_UNIT = MEDICAL_UNIT = st.slider("Medical Unit", 0, 13, step=1)
 
-PATIENT_TYPE = st.radio("PATIENT_TYPE", ['Return Home', 'Hospitalization'])
-if PATIENT_TYPE == 'Return Home':
+PATIENT_TYPE = st.radio("Tipe Pasien", ['Pulang ke rumah', 'Rawat Inap'])
+if PATIENT_TYPE == 'Pulang ke rumah':
      PATIENT_TYPE = 1.0
 else:
     PATIENT_TYPE = 2.0
 
-PNEUMONIA = st.radio("PNEUMONIA", ['Yes', 'No'])
-if PNEUMONIA == 'Yes':
+PNEUMONIA = st.radio("PNEUMONIA", ['Ya', 'Tidak'])
+if PNEUMONIA == 'Ya':
     PNEUMONIA = 1.0
 else:
     PNEUMONIA = 2.0
 
-DIABETES = st.radio("DIABETES", ['Yes', 'No'])
-if DIABETES == 'Yes':
+DIABETES = st.radio("DIABETES", ['Ya', 'Tidak'])
+if DIABETES == 'Ya':
     DIABETES = 1.0
 else:
     DIABETES = 2.0
 
-COPD = st.radio("COPD", ['Yes', 'No'])
-if COPD == 'Yes':
+COPD = st.radio("COPD", ['Ya', 'Tidak'])
+if COPD == 'Ya':
     COPD = 1.0
 else:
     COPD = 2.0
 
-ASTHMA = st.radio("ASTHMA", ['Yes', 'No'])
-if ASTHMA == 'Yes':
+ASTHMA = st.radio("ASTHMA", ['Ya', 'Tidak'])
+if ASTHMA == 'Ya':
     ASTHMA = 1.0
 else:
     ASTHMA = 2.0
 
-INMSUPR = st.radio("INMSUPR", ['Yes', 'No'])
-if INMSUPR == 'Yes':
+INMSUPR = st.radio("IMUNOSUPRESI", ['Ya', 'Tidak'])
+if INMSUPR == 'Ya':
     INMSUPR = 1.0
 else:
     INMSUPR = 2.0
 
-HIPERTENSION = st.radio("HIPERTENSION", ['Yes', 'No'])
-if HIPERTENSION == 'Yes':
+HIPERTENSION = st.radio("HIPERTENSION", ['Ya', 'Tidak'])
+if HIPERTENSION == 'Ya':
     HIPERTENSION = 1.0
 else:
     HIPERTENSION = 2.0
 
-OTHER_DISEASE = st.radio("OTHER_DISEASE", ['Yes', 'No'])
-if OTHER_DISEASE == 'Yes':
+OTHER_DISEASE = st.radio("Penyakit Lain", ['Ya', 'Tidak'])
+if OTHER_DISEASE == 'Ya':
     OTHER_DISEASE = 1.0
 else:
     OTHER_DISEASE = 2.0
 
-CARDIOVASCULAR = st.radio("CARDIOVASCULAR", ['Yes', 'No'])
-if CARDIOVASCULAR == 'Yes':
+CARDIOVASCULAR = st.radio("CARDIOVASCULAR", ['Ya', 'Tidak'])
+if CARDIOVASCULAR == 'Ya':
     CARDIOVASCULAR = 1.0
 else:
     CARDIOVASCULAR = 2.0
 
-OBESITY = st.radio("OBESITY", ['Yes', 'No'])
-if OBESITY == 'Yes':
+OBESITY = st.radio("OBESITAS", ['Ya', 'Tidak'])
+if OBESITY == 'Ya':
     OBESITY = 1.0
 else:
     OBESITY = 2.0
 
-RENAL_CHRONIC = st.radio("RENAL_CHRONIC", ['Yes', 'No'])
-if RENAL_CHRONIC == 'Yes':
+RENAL_CHRONIC = st.radio("Penyakit GINJAL", ['Ya', 'Tidak'])
+if RENAL_CHRONIC == 'Ya':
     RENAL_CHRONIC = 1.0
 else:
     RENAL_CHRONIC = 2.0
 
-TOBACCO = st.radio("TOBACCO", ['Yes', 'No'])
-if TOBACCO == 'Yes':
+TOBACCO = st.radio("Pengguna Tembakau", ['Ya', 'Tidak'])
+if TOBACCO == 'Ya':
     TOBACCO = 1.0
 else:
     TOBACCO = 2.0
 
-COVID_CARRIER=st.radio("COVID_CARRIER",['Yes','No'])
+COVID_CARRIER=st.radio("COVID CARRIER",['Yes','No'])
 if COVID_CARRIER == 'Yes':
     COVID_CARRIER = 1.0
 else:
